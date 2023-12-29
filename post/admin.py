bb6094ed925cdc3cd08742e74332f9958a0fa468
Sina <sina.mehrabi1990@gmail.com>
2023-12-29 18:42:48 +0330

    Add admin models

--- post/admin.py
+++ post/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
+from django.contrib.admin import ModelAdmin
 
-# Register your models here.
+from post.models import Post, UserRating
+
+
+class PostAdmin(ModelAdmin):
+    list_display = ('id', 'title', 'content', 'average_rating', 'total_ratings', 'created_at', 'updated_at')
+
+
+class UserRateAdmin(ModelAdmin):
+    list_display = ('id', 'user', 'post', 'created_at', 'updated_at')
+
+
+admin.site.register(Post, PostAdmin)
+admin.site.register(UserRating, UserRateAdmin)
