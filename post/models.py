1c13b9bf65805bc2b9e2ae0b4bd50fe88453b07f
Sina <sina.mehrabi1990@gmail.com>
2023-12-29 17:23:16 +0330

    Add post and user rating models

--- post/models.py
+++ post/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
-# Create your models here.
+
+class Post(models.Model):
+    title = models.CharField(max_length=100)
+    content = models.TextField()
+
+
+class UserRating(models.Model):
+    user = models.ForeignKey('auth.User', on_delete=models.CASCADE)
+    post = models.ForeignKey(Post, on_delete=models.CASCADE)
+    rating = models.IntegerField(default=0, choices=[(i, str(i)) for i in range(6)])
+
+    class Meta:
+        unique_together = ('user', 'post')
