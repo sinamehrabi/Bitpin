1734da2e64e8a44de79eb1c3f51c6d8b41bdd5c3
Sina <sina.mehrabi1990@gmail.com>
2023-12-29 18:41:41 +0330

    delete unique together attribute for user rate and check it in code

--- post/models.py
+++ post/models.py
@@ -9,11 +9,12 @@ class Post(BaseModel):
     total_ratings = models.IntegerField(default=0)
     average_rating = models.DecimalField(max_digits=3, decimal_places=2, default=0.00)
 
+    def __str__(self):
+        return self.title
+
 
 class UserRating(BaseModel):
     user = models.ForeignKey('auth.User', on_delete=models.CASCADE)
     post = models.ForeignKey(Post, on_delete=models.CASCADE)
     rating = models.IntegerField(default=0, choices=[(i, str(i)) for i in range(6)])
 
-    class Meta:
-        unique_together = ('user', 'post')
