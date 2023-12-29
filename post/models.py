c79f5e37d4c75fcca0064867b193b47652f7e437
Sina <sina.mehrabi1990@gmail.com>
2023-12-29 17:49:56 +0330

    Add base model with created_at and updated_at fields

--- post/models.py
+++ post/models.py
@@ -1,14 +1,16 @@
 from django.db import models
 
+from bitpin.base_model import BaseModel
 
-class Post(models.Model):
+
+class Post(BaseModel):
     title = models.CharField(max_length=100)
     content = models.TextField()
     total_ratings = models.IntegerField(default=0)
     average_rating = models.DecimalField(max_digits=3, decimal_places=2, default=0.00)
 
 
-class UserRating(models.Model):
+class UserRating(BaseModel):
     user = models.ForeignKey('auth.User', on_delete=models.CASCADE)
     post = models.ForeignKey(Post, on_delete=models.CASCADE)
     rating = models.IntegerField(default=0, choices=[(i, str(i)) for i in range(6)])
